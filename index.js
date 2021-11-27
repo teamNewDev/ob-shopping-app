3746d9502aefa9d10ce1b9725ee0d46b25463420
Blaire <ogoh_blessing@yahoo.com>
2021-11-27 13:06:32 -0800

    Fixed a couple of syntax errors

--- index.js
+++ index.js
@@ -1,14 +1,59 @@
-const db = new Dexie('ShoppingApp')
-db.version(1).stores( e, { items: '++id,name,price,isPurchased'})
+const db = new Dexie('ShoppingApp');
+db.version(1).stores( { items: '++id,name,price,isPurchased'} );
 
-const itemForm = document.getElementById('itemForm')
-const itemsDiv = document.getElementById('itemsDiv')
-const totalPriceDiv = document.getElementById('totalPriceDiv')
+const itemForm = document.getElementById('itemForm');
+const itemsDiv = document.getElementById('itemsDiv');
+const totalPriceDiv = document.getElementById('totalPriceDiv');
+
+const populateItemsDiv = async () => {
+    const allItems = await db.items.reverse().toArray()
+    /*[
+        {
+            name: 'hello',
+            quantity: 4,
+            price: 10,
+            isPurchased: true
+        },
+        {
+            name: 'Milk',
+            quantity: 1,
+            price: 10,
+            isPurchased: false
+        }
+    ]*/
+
+    itemsDiv.innerHTML = allItems.map( item => `
+        <div class="item ${item.isPurchased && 'purchased'}">
+            <label>
+                <input 
+                type="checkbox" 
+                class="checkbox" 
+                ${item.isPurchased && 'checked'}>
+            </label>
+
+            <div class="itemInfo">
+                <p>${item.name}</p>
+                <p>$${item.price} x ${item.quantity}</p>
+            </div>
+
+            <button class="deleteButton">X</button>
+        </div>
+    `)
+
+    const arrayOfPrices = allItems.map(item => item.price * item.quantity);
+    const totalPrice = arrayOfPrices.reduce((a, b) => a + b, 0);
+    
+    totalPriceDiv.innerText = 'Total price: $'+ totalPrice;
+}
 
 itemForm.onsubmit = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    const name = document.getElementById('nameInput').value
-    const quantity = document.getElementById('quantityInput').value
-    const price = document.getElementById('priceInput').value
-}
+    const name = document.getElementById('nameInput').value;
+    const quantity = document.getElementById('quantityInput').value;
+    const price = document.getElementById('priceInput').value;
+
+    await db.items.add({ name, quantity, price });
+
+    itemForm.reset();
+};
